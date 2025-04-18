cf3194f19332610dc71148931a1920dee4a2b00f
Ayush Kushwaha <58135144+ayushkx@users.noreply.github.com>
2025-04-18 15:14:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
- access_key = “YOUR_ACCESS_KEY”
-  secret_key = "YOUR_SECRET_KEY"
+ access_key = var.Access_key_aws
+  secret_key = var.Secret_key_aws
 }
 
 resource "aws_instance" "swanand-instance" {
