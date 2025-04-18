f6cdc84862bed5dc0c88271633dbb16d0c6b710d
Ayush Kushwaha <58135144+ayushkx@users.noreply.github.com>
2025-04-18 15:12:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "<5.94.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+ access_key = “YOUR_ACCESS_KEY”
+  secret_key = "YOUR_SECRET_KEY"
+}
+
+resource "aws_instance" "swanand-instance" {
+        ami = var.ami-id
+        instance_type = var.instance-type
+        availability_zone = var.az
+        key_name = "ayush_terraform"
+        tags = {
+                Name = var.instance-name
+}
+}
+
+
+
 
