fabe424941b9ce4d2a5933b4f1d578495aa2ac9b
Ayush Kushwaha <58135144+ayushkx@users.noreply.github.com>
2025-04-18 15:21:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
- access_key = var.Access_key_aws
-  secret_key = var.Secret_key_aws
+ access_key = Access_key_aws
+  secret_key = Secret_key_aws
 }
 
 resource "aws_instance" "RHEL_AMI" {
